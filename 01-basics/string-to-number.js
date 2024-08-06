e134a46d76fbb42002bbb47736d2be2d65f939a0
Sumit Kumar Roy <127715512+Sumitgitup@users.noreply.github.com>
2024-08-06 16:49:12 +0000

    conversion done

---
+++ 01-basics/string-to-number.js
@@ -0,0 +1,11 @@
+
+
+// console.log(+true)
+// console.log(+"")
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
+
+// learn more about prefix and postfix from the below
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
