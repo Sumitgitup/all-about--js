734624ba18757168fe85391296b47418edda149f
Sumit Kumar Roy <127715512+Sumitgitup@users.noreply.github.com>
2024-08-08 13:37:33 +0000

    maths and nums done

---
+++ 01-basics/nums_and_math.js
@@ -0,0 +1,16 @@
+// const score = 400;
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance)
+
+// console.log(balance.toFixed(3));
+// const otherNumber = 1000000
+// console.log(otherNumber.toLocaleString('en-IN'));
+
+
+// -------------------------------------------------------------------------------------------------------
+
+console.log(Math)
+console.log(Math.abs(-9898989));
+
