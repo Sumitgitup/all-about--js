82a896a770797ac909b723e3c6423931b7566896
Sumit Kumar Roy <127715512+Sumitgitup@users.noreply.github.com>
2024-08-11 12:01:11 +0000

    reduce Function

--- 01-basics/reduce.js
+++ 01-basics/reduce.js
@@ -1,9 +1,36 @@
-const myNums = [1, 2, 3]
+// Using reduce function 
+// const myNums = [1, 2, 3]
 
-const reult = myNums.reduce((acc, currVal) => {
-    console.log(`acc: ${acc} and currVal: ${currVal}`);
+// const reult = myNums.reduce((acc, currVal) => {
+//     console.log(`acc: ${acc} and currVal: ${currVal}`);
     
-    return acc + currVal
+//     return acc + currVal
+// }, 0)
+
+// console.log(reult)
+
+
+const shoppingCart = [
+    {
+        item: 'js course',
+        price: 3999
+    },
+    {
+        item: 'web dev course',
+        price: 3999
+    },
+    {
+        item: 'Web 3 course',
+        price: 3999
+    },
+    {
+        item: 'Mobile Dev course',
+        price: 3999
+    }
+]
+const priceToPay = shoppingCart.reduce((acc, item) => {
+    return acc + item.price
 }, 0)
 
-console.log(reult)
+console.log(priceToPay);
+
